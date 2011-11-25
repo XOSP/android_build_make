1cdae293a89518d71f1c2009d97d325faae22a73
Ricardo Cerqueira <github@cerqueira.org>
2011-11-25 15:30:36 +0000

    build: XOSP Builds bring up
    Signed-off-by: Calin <nilac8991@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -128,6 +128,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^cm_") ; then
+       XOSP_BUILD=$(echo -n $1 | sed -e 's/^xosp_//g')
+    else
+       XOSP_BUILD=
+    fi
+    export XOSP_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
