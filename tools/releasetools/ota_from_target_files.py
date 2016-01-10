239e69bca9c880ccbc927f8491105584082e7c74
Calin <nilac8991@gmail.com>
2016-01-10 19:23:03 +0100

    Hide this for now
    Signed-off-by: Calin <nilac8991@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -645,14 +645,14 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Unmount("/data")
   script.AppendExtra("endif;")
 
-script.Print("")  
-script.Print("____  ___________    ___________________")  
-script.Print("\   \/  /\_____  \  /   _____/\______   \ ") 
-script.Print(" \     /  /   |   \ \_____  \  |     ___/") 
-script.Print(" /     \ /    |    \/        \ |    |   ")  
-script.Print("/___/\  \\_______  /_______  / |____|   ")  
-script.Print("      \_/        \/        \/           ")
-script.Print("")
+#script.Print("")  
+#script.Print("____  ___________    ___________________")  
+#script.Print("\   \/  /\_____  \  /   _____/\______   \ ") 
+#script.Print(" \     /  /   |   \ \_____  \  |     ___/") 
+#script.Print(" /     \ /    |    \/        \ |    |   ")  
+#script.Print("/___/\  \\_______  /_______  / |____|   ")  
+#script.Print("      \_/        \/        \/           ")
+#script.Print("")
 
   if "selinux_fc" in OPTIONS.info_dict:
     WritePolicyConfig(OPTIONS.info_dict["selinux_fc"], output_zip)
