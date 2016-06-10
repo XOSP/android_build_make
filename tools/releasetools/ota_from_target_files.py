f7f534f94af4197817b7b8d2f850f8a0e5b0053a
Calin <nilac8991@gmail.com>
2016-06-10 23:45:00 +0200

    Flash XOSPDelta during the ROM installation
    - Added necessary commands to be executed after the ROM flashing process
    
    Signed-off-by: Calin <nilac8991@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -776,6 +776,16 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     if block_based:
       script.Unmount("/system")
 
+
+  if block_based:
+    script.Print("Preparing XOSPDelta...")
+    common.ZipWriteStr(output_zip, "install/xospdelta/XOSPDelta.zip",
+                   ""+input_zip.read("INSTALL/xospdelta/XOSPDelta.zip"))
+    script.Mount("/system")
+    script.InstallXOSPDelta()
+  if block_based:
+    script.Unmount("/system")
+
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
