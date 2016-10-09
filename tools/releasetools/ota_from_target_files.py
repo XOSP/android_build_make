cbcb70de653be8096b5a9e9a71d534fff69f030e
Josh Chasky <jchasky@gmail.com>
2016-10-09 01:07:02 +0000

    Flash SuperSU with rom [2/2]
    Conflicts:
    	tools/releasetools/edify_generator.py

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -774,6 +774,12 @@ script.Print("")
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
+  if block_based:
+    script.Print("Flashing SuperSU...")
+    common.ZipWriteStr(output_zip, "supersu/supersu.zip",
+    ""+input_zip.read("SYSTEM/addon.d/UPDATE-SuperSU.zip"))
+    script.FlashSuperSU()
+
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
 
