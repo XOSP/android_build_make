28ef6d1742e90ce95e73475eb9b3d9fd5a213622
Calin <nilac8991@gmail.com>
2017-04-04 08:23:41 +0200

    Revert "Revert "Add magisk built in flashing support [2/2]""
    This reverts commit d4b256e938c02d0b9c3ee996857f4fb3bfe64433.

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -780,6 +780,12 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
       common.ZipWriteStr(output_zip, "supersu/supersu.zip",
       ""+input_zip.read("SYSTEM/addon.d/UPDATE-SuperSU.zip"))
       script.FlashSuperSU()
+	  
+    if os.getenv('WITH_MAGISK','true') != 'false' :
+      script.Print("Flashing Magisk...")
+      common.ZipWriteStr(output_zip, "magisk/magisk.zip",
+      ""+input_zip.read("SYSTEM/addon.d/Magisk.zip"))
+      script.FlashMagisk()
 
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
